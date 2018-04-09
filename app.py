0e6b4a10727eb22505be9624e745c2fd50f6dd35
Mel <mmgravier@gmail.com>
2018-04-09 13:18:21 -0400

    added logos, working

--- app.py
+++ app.py
@@ -5,18 +5,26 @@ from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisisasecretkey!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/mgravier/Desktop/app/database.db'
 Bootstrap(app)
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-class User(db.Model):
+class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(15), unique=True)
 	email = db.Column(db.String(50), unique=True)
 	password = db.Column(db.String(80), unique=True)
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
 
 class LoginForm(FlaskForm):
 	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -43,6 +51,7 @@ def login():
 		user = User.query.filter_by(username=form.username.data).first() 
 		if user:
 			if check_password_hash(user.password, form.password.data):
+				login_user(user, remember=form.remember.data)
 				return redirect(url_for('dashboard'))
 
 		return '<h1>Invalid username or password</h1>'
@@ -69,8 +78,15 @@ def signup():
 	return render_template('signup.html', form=form)
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html', name=current_user.username)
+
+@app.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
